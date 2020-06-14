35e6bead4ed81668b96e33644be3424e150a2bd7
sp00nBoy <emreinchains@gmail.com>
2020-06-15 01:31:59 +0300

    send

---
+++ app/src/main/java/com/example/nerdeyesem/LoginActivity.java
@@ -0,0 +1,29 @@
+package com.example.nerdeyesem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    ImageView backButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        backButton = findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+}
